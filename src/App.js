a528529ce976b65ef496340a13ee7fb13e2e1aa3
Zoyasajid <sajidzoya72@gmail.com>
2023-08-05 13:13:50 +0500

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React,{useState} from 'react'
-// import './App.css';
+import './App.css';
+import {FaTrashAlt} from 'react-icons/fa'
 let globalid=0
 function App() {
 const [task,checktask]=useState("")
 const [todo , todoupdate ]=useState([ ])
 function click(){
+  
   todoupdate(oldtask=>{
+    
     checktask('')
-    // return [...oldtask ,task]
     return [...oldtask ,{todo:task,id:globalid++}]
   })
 }
@@ -19,20 +21,24 @@ function enterkey(er){
 if (er.keyCode===13){
   click()
 }}
-  return <div style={{backgroundColor: "purple"}}>
-    <h1>TO DO LIST</h1>
-    <input 
+  return <div className='main-div'>
+    <div className='field'>
+    <div className='inputfield'>
+    <h1><i>--TO-DO LIST--</i></h1>
+    <input className='text'
     onKeyDown={enterkey}
-     type="text" 
-     value={task} 
-     onChange={e=>checktask(e.target.value)}/>
-    <button onClick={click}>Click to add</button>
- 
+    type="text" 
+    value={task} 
+    onChange={e=>checktask(e.target.value)}/>
+    </div>
+    <button onClick={click} className='btn'>Click to add</button>
+    <hr/>
+ </div>
     <ul>
       {todo.map((item,index)=>{
-      return <div key={index}>
+      return <div className='select' key={index}>
         <li>{item.todo}</li>
-        <button onClick={()=>Deletetheitem(item.id)}>Delete</button>
+        <button className='delete' onClick={()=>Deletetheitem(item.id)}><FaTrashAlt/></button>
 
        </div>
 })}
